1a1ce4cdb5b0814656b3d079011471e4cd052330
xingxing.lv <xingxing.lv@megatronix.co>
2022-10-25 10:32:28 +0800

    modify jacoco setting

--- app/src/main/java/com/dream/create/framework/core/platform/BaseBean.kt
+++
@@ -1,9 +0,0 @@
-package com.wmm.wanandroid.core.platform
-import java.io.Serializable
-
-
-class BaseBean<T> : Serializable {
-    var errorCode = -91275
-    var errorMsg: String? = null
-    internal var data: T? = null
-}
